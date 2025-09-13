e4a36b497304ea66be311820f450e67418eceb17
Dyno <dvghule121@gmail.com>
2025-09-13 06:43:34 +0530

    chore: update hosted frontend link in cors

--- backend/settings.py
+++ backend/settings.py
@@ -153,6 +153,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://pave-asignment-frontend.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
